bbec8bfb6c562faa546574f977849ca550aa2efb
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2024-12-03 21:28:43 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# merge-images
+
+NodeJS script for merging images horizontally to one PNG.
+
+## Requirments
+
+* node@20
+
+## How to use
+
+1. Install dependencies with `npm i`.
+1. Place input images to `input` directory.
+1. Execute `node index`. Merged image will be placed to `output` directory.
---
+++ index.js
@@ -0,0 +1,28 @@
+const { Canvas, loadImage } = require('canvas');
+const CanvasGrid = require('merge-images-grid');
+const fs = require('fs');
+const fse = require('fs-extra');
+
+const dir = './input/';
+const listFiles = fs.readdirSync(dir);
+
+async function work() {
+  const list = await Promise.all(
+	    listFiles.map(async (f) => ({
+	       image: await loadImage(dir + f),
+	    }))
+	);
+  const merge = new CanvasGrid({
+    canvas: new Canvas(list.length, 1),
+    list,
+    gap: 0,
+    padding: 0,
+  })
+  const buffer = merge.canvas.toBuffer();
+  fse.outputFileSync(`./output/${Date.now()}.png`, buffer);
+}
+
+work()
+.catch(e => {
+	console.error(e);
+})
